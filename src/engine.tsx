2809c5e69c161f45f81c83f5483b3909314a3169
G4mingJon4s <Jthelker@gmail.com>
2023-08-19 23:43:10 +0200

    added sleeve bonus

--- src/engine.tsx
+++ src/engine.tsx
@@ -112,7 +112,7 @@ const Engine: {
     // Bladeburner
     if (Player.bladeburner) Player.bladeburner.storeCycles(numCycles);
 
-		if (Player.worm) Player.worm.process();
+		if (Player.worm) Player.worm.process(numCycles);
 
     // Sleeves
     Player.sleeves.forEach((sleeve) => sleeve.process(numCycles));
@@ -327,7 +327,7 @@ const Engine: {
       // Bladeburner offline progress
       if (Player.bladeburner) Player.bladeburner.storeCycles(numCyclesOffline);
 
-			if (Player.worm) Player.worm.process();
+			if (Player.worm) Player.worm.process(numCyclesOffline);
 			
       staneksGift.process(numCyclesOffline);
 
