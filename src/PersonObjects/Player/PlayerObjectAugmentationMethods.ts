20ddae677b8263762422518ea0a820330ca8e84d
G4mingJon4s <Jthelker@gmail.com>
2023-08-19 14:51:24 +0200

    queued aug bonus, aug ui excludes mechanic mults

--- src/PersonObjects/Player/PlayerObjectAugmentationMethods.ts
+++ src/PersonObjects/Player/PlayerObjectAugmentationMethods.ts
@@ -1,16 +1,20 @@
 /** Augmentation-related methods for the Player class (PlayerObject) */
+import { Multipliers } from "@nsdefs";
 import { calculateEntropy } from "../Grafting/EntropyAccumulation";
 import { staneksGift } from "../../CotMG/Helper";
 import { updateGoMults } from "../../Go/effects/effect";
 
 import type { PlayerObject } from "./PlayerObject";
 
-export function applyEntropy(this: PlayerObject, stacks = 1): void {
+export function applyEntropy(this: PlayerObject, stacks = 1): Multipliers {
   // Re-apply all multipliers
   this.reapplyAllAugmentations();
   this.reapplyAllSourceFiles();
 
-  this.mults = calculateEntropy(stacks);
+  const entropyMults = calculateEntropy(stacks);
+  this.mults = entropyMults;
   staneksGift.updateMults();
   updateGoMults();
+
+	return entropyMults;
 }
