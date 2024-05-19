7888765d7eaaca530bee9f0df76204291304d22f
G4mingJon4s <Jthelker@gmail.com>
2024-05-19 11:51:20 +0200

    added contract effect

--- src/Worm/ui/WormOverview.tsx
+++ src/Worm/ui/WormOverview.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Typography } from "@mui/material";
+import { Button, Divider, Tooltip, Typography } from "@mui/material";
 import { Theme } from "@mui/material/styles";
 import React, { useState } from "react";
 import { Worm } from "../Worm";
@@ -6,6 +6,9 @@ import { formatNumber, formatPercent } from "../../ui/formatNumber";
 import { WormBonusModal } from "./WormBonusModal";
 import { formatBonusDescription, getBonusEffect } from "../BonusType";
 import { makeStyles } from "@mui/styles";
+import { MathJax } from "better-react-mathjax";
+import { wormContractEffect } from "../calculations";
+import { Player } from "@player";
 
 interface IProps {
 	worm: Worm;
@@ -20,11 +23,21 @@ const overviewStyles = makeStyles((theme: Theme) => ({
 	},
 	completionsText: {
 		color: theme.colors.successlight
-	}
+	},
+	stackContainer: {
+		display: "flex",
+		flexDirection: "row",
+	},
+	stackSegment: {
+		width: "calc(100% - 25px)",
+		marginInline: "10px"
+	},
 }));
 
 export function WormOverview({ worm }: IProps) {
 	const [bonusModalOpen, setBonusModalOpen] = useState(false);
+
+	const contractEffect = wormContractEffect(Player.numContractsSolved);
 
 	const classes = overviewStyles();
 
@@ -42,17 +55,41 @@ export function WormOverview({ worm }: IProps) {
         It is your task to develop a program that can solve the networks properties as efficient as possible.
 			</Typography>
 			<Divider sx={{ my: 1.5 }}/>
-			<Typography variant="h5">Bonus</Typography>
-			<Typography className={classes.completionsText}>Current Completions: {formatNumber(worm.completions)}</Typography>
-			<br />
-			<Typography>Current Bonus: {worm.bonus.name} (#{worm.bonus.id})</Typography>
-			<Typography className={classes.effectText}>
-				Effect: {formatBonusDescription(getBonusEffect(worm.bonus, worm.completions), worm.bonus.description)}
-			</Typography>
-			<Typography className={classes.maximumText}>Maximum: {formatPercent(worm.bonus.g)}</Typography>
-			<br />
-			<WormBonusModal worm={worm} open={bonusModalOpen} onClose={() => setBonusModalOpen(false)}/>
-			<Button onClick={() => setBonusModalOpen(true)}>Change Bonus</Button>
+			<div className={classes.stackContainer}>
+				<div className={classes.stackSegment}>
+					<Typography variant="h5">Bonus</Typography> <Typography className={classes.completionsText}>Current Completions: {formatNumber(worm.completions)}</Typography>
+					<br />
+					<Typography>Current Bonus: {worm.bonus.name} (#{worm.bonus.id})</Typography>
+					<Typography className={classes.effectText}>
+						Effect: {formatBonusDescription(getBonusEffect(worm.bonus, worm.completions), worm.bonus.description)}
+					</Typography>
+					<Typography className={classes.maximumText}>Maximum: {formatPercent(worm.bonus.g)}</Typography>
+					<br />
+					<WormBonusModal worm={worm} open={bonusModalOpen} onClose={() => setBonusModalOpen(false)}/>
+					<Button onClick={() => setBonusModalOpen(true)}>Change Bonus</Button>
+				</div>
+				<Divider orientation="vertical" flexItem/>
+				<div className={classes.stackSegment}>
+					<Typography variant="h5">Contract influence</Typography>
+					<Typography>
+						The more coding contracts you solve, the faster your inputs get processed by the Worm.
+					</Typography>
+					<br />
+					<Typography>Solved Coding Contracts: {Player.numContractsSolved}</Typography>
+					<Tooltip
+						title={
+							<>
+								<Typography>Multiplier formula:</Typography>
+								<MathJax>{"\\(\\huge{M = e^{- \\log{\\left(\\frac{x}{200} + 1\\right)}}}\\)"}</MathJax>
+							</>
+						}
+					>
+						<Typography className={classes.effectText}>
+							Current effect: -{formatPercent(1 - contractEffect)} processing time required
+						</Typography>
+					</Tooltip>
+				</div>
+			</div>
 		</>
 	)
 }
